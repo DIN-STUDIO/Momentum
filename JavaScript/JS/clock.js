e6d6bf1f6e4943db4e7a43531e502bd3e981d03e
DIN-STUDIO <86708287+DIN-STUDIO@users.noreply.github.com>
2021-08-02 22:40:48 +0900

    JS Study [21.08.02]
    - studied how to use 'JSON.stringifiy()'
    - studied 'event.target'
    - studied relationship of 'parent-child'

--- JavaScript/JS/clock.js
+++ JavaScript/JS/clock.js
@@ -3,12 +3,14 @@ const clock = document.querySelector("#clock")
 function getClock() {
     const time = new Date()
 
+    const year = time.getFullYear()
+    const month = String(time.getMonth()).padStart(2, "0")
+    const date = String(time.getDate()).padStart(2, "0")
     const hour = String(time.getHours()).padStart(2, "0")
     const minute = String(time.getMinutes()).padStart(2, "0")
     const second = String(time.getSeconds()).padStart(2, "0")
 
-    clock.innerText = hour + " : " + minute + " : " + second
+    clock.innerText = `[ ${year} / ${month} / ${date} ] [ ${hour} : ${minute} : ${second} ]`
 }
-
 getClock()
 setInterval(getClock, 1000)
--- JavaScript/JS/todo.js
+++ JavaScript/JS/todo.js
@@ -2,10 +2,46 @@ const toDoForm = document.querySelector("#todoForm")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todoList")
 
+const greet = document.querySelector("#greeting")
+const savingTodo = []
+
+function saveTodo() {
+    localStorage.setItem("todoItems", JSON.stringify(savingTodo))
+}
+
+function deleteButton(event) {
+    const li = event.target.parentElement
+    li.remove()
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    span.innerText = newTodo
+    
+    const button = document.createElement("button")  
+    button.innerText = "Delete"
+
+    li.appendChild(span)
+    li.appendChild(button)
+    toDoList.appendChild(li)
+
+    button.addEventListener("click", deleteButton)
+}
+
 function handelToDo(event) {
     event.preventDefault()
-    const inputText = toDoInput.value
+    const newTodo = toDoInput.value
     toDoInput.value = ""
-    console.log(inputText)
+    savingTodo.push(newTodo)
+    paintTodo(newTodo)
+    saveTodo()
+
+    // console.log(savingTodo)
+
+    
+    const abcd = localStorage.getItem("todoItems")
+    greet.innerText = "To Do is " + abcd
+    
 }
 toDoForm.addEventListener("submit", handelToDo)
