08a9a96ad18b53f3c88377e8c03b5de17beb465b
DIN-STUDIO <86708287+DIN-STUDIO@users.noreply.github.com>
2021-08-04 15:22:40 +0900

    JS Study [21.08.04]
    - studied deleting local storage's todolists.
    - studied saving todolists as object.
    - studied forEach function, filter function.

--- JavaScript/JS/todo.js
+++ JavaScript/JS/todo.js
@@ -3,7 +3,8 @@ const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todoList")
 
 const greet = document.querySelector("#greeting")
-const savingTodo = []
+let savingTodo = []
+const TODOS_KEY = "todoItems"
 
 function saveTodo() {
     localStorage.setItem("todoItems", JSON.stringify(savingTodo))
@@ -12,12 +13,16 @@ function saveTodo() {
 function deleteButton(event) {
     const li = event.target.parentElement
     li.remove()
+    savingTodo = savingTodo.filter((items) => items.id != parseInt(li.id))
+    saveTodo()
+    console.log(li.id)
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement("li")
     const span = document.createElement("span")
-    span.innerText = newTodo
+    span.innerText = newTodo.text
+    li.id = newTodo.id
     
     const button = document.createElement("button")  
     button.innerText = "Delete"
@@ -33,15 +38,21 @@ function handelToDo(event) {
     event.preventDefault()
     const newTodo = toDoInput.value
     toDoInput.value = ""
-    savingTodo.push(newTodo)
-    paintTodo(newTodo)
-    saveTodo()
-
-    // console.log(savingTodo)
-
-    
-    const abcd = localStorage.getItem("todoItems")
-    greet.innerText = "To Do is " + abcd
-    
+    let todoObj = {
+        text : newTodo,
+        id : Date.now()
+    }
+    savingTodo.push(todoObj)
+    paintTodo(todoObj)
+    saveTodo()    
 }
+
 toDoForm.addEventListener("submit", handelToDo)
+
+const savedTodos = localStorage.getItem(TODOS_KEY)
+
+if (savedTodos != null) {
+    const parseTodos = JSON.parse(savedTodos)   // "parseTodos" is an array!
+    savingTodo = parseTodos
+    parseTodos.forEach(paintTodo);
+}
