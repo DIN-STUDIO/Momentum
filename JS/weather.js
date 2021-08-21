8e75109920af75c623b0f991f1868cfe9bfe6631
DIN-STUDIO <86708287+DIN-STUDIO@users.noreply.github.com>
2021-08-21 23:53:21 +0900

    Momentum [21.08.21]
    - studied how to load weather, city name information

--- JS/weather.js
+++ JS/weather.js
@@ -1,10 +1,16 @@
-function onGeoOk(position) {
-    console.log(position)
+const API_KEY = "cf3281b29439f18a63b476afd82f20fd"
 
+function onGeoOk(position) {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
+    const geoUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
 
-    console.log("You live in ", lat, long)
+    fetch(geoUrl).then(response => response.json()).then(data => {
+        const cityName = document.querySelector("#weather span:first-child")
+        const weather = document.querySelector("#weather span:last-child")
+        cityName.innerText = `City : ${data.name}  /  `;
+        weather.innerText = `Weather : ${data.weather[0].main}  /  Temp : ${data.main.temp}`;
+    })
 }
 
 function onGeoError() {
